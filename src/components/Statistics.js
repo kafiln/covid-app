53c365551517c015c5b35aa866423319311b2e6d
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-04-14 01:36:42 +0100

    Including tailwind-css

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -15,9 +15,9 @@ function Statistics({
         <FormattedMessage id={KEYS.LAST_UPDATED}></FormattedMessage>
         <FormattedDate value={new Date(lastUpdate)}></FormattedDate>
       </h2>
+      <StatisticCard stats="CONFIRMED" number={confirmed}></StatisticCard>
       <StatisticCard stats="RECOVERED" number={recovered}></StatisticCard>
       <StatisticCard stats="DEATHS" number={deaths}></StatisticCard>
-      <StatisticCard stats="CONFIRMED" number={confirmed}></StatisticCard>
       <StatisticCard stats="NEGATIVES" number={negatives}></StatisticCard>
     </div>
   );
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FormattedMessage } from 'react-intl';
+import { ThemeContext } from 'styled-components';
 import { KEYS } from '../i18n';
 
 const Title = () => {
+  const theme = useContext(ThemeContext);
   return (
-    <>
-      <h1>
-        <span role="img" aria-label="Virus">
+    <div
+      className={`max-w-lg mx-auto p-6 flex flex-col bg-${theme.backgroundColor} rounded-lg shadow-sm`}
+    >
+      <h1 className="text-2xl mb-2">
+        <span className="mr-1" role="img" aria-label="Virus">
           🦠
         </span>
         <FormattedMessage id={KEYS.TITLE}></FormattedMessage>
       </h1>
-      <p>
-        {' '}
+      <p className="text-md">
         <FormattedMessage id={KEYS.SUBTITLE}></FormattedMessage>
       </p>
-    </>
+    </div>
   );
 };
 
