84c40980cdaf126dbc53ef56d3e451a34303190a
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-04-21 03:46:52 +0000

    removing unused css

---
+++ craco.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const fs = require('fs');
+const glob = require('glob-all');
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
+
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+
+module.exports = {
+  webpack: {
+    plugins: [
+      new PurgecssPlugin({
+        paths: [
+          resolveApp('public/index.html'),
+          ...glob.sync(`${resolveApp('src')}/**/**/*`, { nodir: true }),
+        ],
+      }),
+    ],
+  },
+};
