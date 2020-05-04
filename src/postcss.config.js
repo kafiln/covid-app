ee1cec08e080fbb810c8e71a288b59d608f297f4
Mohamed ELBARCHANY <elbarchany@gmail.com>
2020-05-04 20:31:02 +0000

    Reduce CSS chunk by activating purging unused css (#7)
    * Activate css purge for production build
    
    * Clean up config

--- src/postcss.config.js
+++ src/postcss.config.js
@@ -1,16 +1,9 @@
 const tailwindcss = require('tailwindcss');
 
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./src/**/*.js', './src/**/*.html'],
-
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
 
 module.exports = {
   plugins: [
     tailwindcss('./tailwind.js'),
-    require('autoprefixer'),
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    require('autoprefixer')
   ],
 };
