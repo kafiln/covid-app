57eeb7ea6c55920d3e918ca603baf910645f8a99
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-05-08 08:56:09 +0000

    Simplify language toggle

--- src/components/LanguageToggle.js
+++ src/components/LanguageToggle.js
@@ -1,76 +1,30 @@
-import React, { useContext, useState } from 'react';
-import { AiOutlineDown } from 'react-icons/ai';
-import { MdLanguage } from 'react-icons/md';
-import { FormattedMessage } from 'react-intl';
-import styled, { useTheme } from 'styled-components';
+import React, { useContext } from 'react';
 import { AppContext, Types } from '../context';
-import { KEYS, Languages as LOCALES } from '../i18n';
-
-const Dropdown = styled.div`
-  font: 400 16px system-ui;
-  color: ${(props) => props.backgroundColor};
-  background-color: ${(props) => props.color};
-
-  .menu {
-    color: ${(props) => props.backgroundColor};
-    background-color: ${(props) => props.color};
-  }
-`;
+import { Languages as LOCALES } from '../i18n';
 
 function LanguageToggle() {
   const { lang, dispatch } = useContext(AppContext);
-  const { color, backgroundColor } = useTheme();
-  const [hidden, setHidden] = useState(true);
   const languages = Object.values(LOCALES);
-
-  const toggle = () => {
-    setHidden(!hidden);
-  };
+  const displayLanguages = languages.filter((e) => e.id !== lang);
 
   const handleChange = (langId) => {
-    toggle();
     dispatch({
       type: Types.CHANGE_LANGUAGE,
       payload: langId,
     });
   };
   return (
-    <Dropdown
-      color={color}
-      backgroundColor={backgroundColor}
-      className="dropdown rounded flex flex-col cursor-pointer rounded-b-none"
-    >
-      <div onClick={toggle} className="wrapper">
-        <div className="font-semibold py-1 px-2 inline-flex items-center focus:outline-none">
-          <MdLanguage size="1.5em" />
-          <span className="mx-1">
-            <FormattedMessage id={KEYS.LANGUAGES}></FormattedMessage>
-          </span>
-          <AiOutlineDown />
-        </div>
-      </div>
-
-      <div className="relative">
-        <ul
-          className={`menu pt-1 focus:outline-none absolute top-0 left-0 w-full ${
-            hidden && 'hidden'
-          } `}
+    <ul className="flex">
+      {displayLanguages.map((lang) => (
+        <li
+          className="cursor-pointer mx-2 hover:underline font-bold"
+          key={lang.id}
+          onClick={() => handleChange(lang.id)}
         >
-          {languages &&
-            languages
-              .filter((e) => e.id !== lang)
-              .map((language) => (
-                <li
-                  onClick={() => handleChange(language.id)}
-                  key={language.id}
-                  className={`hover:font-bold py-1 px-2 whitespace-no-wrap`}
-                >
-                  {language.displayName}
-                </li>
-              ))}
-        </ul>
-      </div>
-    </Dropdown>
+          {lang.displayName}
+        </li>
+      ))}
+    </ul>
   );
 }
 
