c65c03379ff1121d3d7d2467ca1e2aed9d6d088a
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-06-16 05:09:59 +0100

    Full dark mode

--- src/theme/dark.js
+++ src/theme/dark.js
@@ -2,7 +2,7 @@ export default {
   color: '#FFF',
   backgroundColor: '#292c35',
   statisticCard: {
-    container: 'bg-blue-900 text-gray-300',
+    container: 'bg-gray-800 text-gray-300',
   },
   githubButton: ' bg-black text-white border-white',
   regions: {
